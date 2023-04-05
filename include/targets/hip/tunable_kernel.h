draenog/quda

commit fad144bad59dc173e54f183ef6a7631d517fed87

Simplify constant memory tuneKey labeling to ensure it is ODR safe

include/targets/hip/tunable_kernel.h
@@ -19,6 +19,19 @@ namespace quda
    */
   qudaError_t qudaLaunchKernel(const void *func, const TuneParam &tp, const qudaStream_t &stream, const void *arg);
 
+  /**
+     @brief This helper function indicates if the present
+     compilation unit has explicit constant memory usage enabled.
+  */
+  static bool use_constant_memory()
+  {
+#ifdef QUDA_USE_CONSTANT_MEMORY
+    return true;
+#else
+    return false;
+#endif
+  }
+
   class TunableKernel : public Tunable
   {
 

include/targets/cuda/tunable_kernel.h
@@ -22,6 +22,19 @@ namespace quda
   */
   qudaError_t qudaLaunchKernel(const void *func, const TuneParam &tp, const qudaStream_t &stream, const void *arg);
 
+  /**
+     @brief This helper function indicates if the present
+     compilation unit has explicit constant memory usage enabled.
+  */
+  static bool use_constant_memory()
+  {
+#ifdef QUDA_USE_CONSTANT_MEMORY
+    return true;
+#else
+    return false;
+#endif
+  }
+
   class TunableKernel : public Tunable
   {
 
@@ -82,7 +95,7 @@ namespace quda
     {
       strcpy(vol, field.VolString().c_str());
       strcpy(aux, compile_type_str(field, location));
-      if (this->location == QUDA_CUDA_FIELD_LOCATION && device::use_constant_memory_arg<>::value) strcat(aux, "cmem,");
+      if (this->location == QUDA_CUDA_FIELD_LOCATION && use_constant_memory()) strcat(aux, "cmem,");
       if (this->location == QUDA_CPU_FIELD_LOCATION) strcat(aux, getOmpThreadStr());
       strcat(aux, field.AuxString().c_str());
     }
@@ -91,7 +104,7 @@ namespace quda
     {
       u64toa(vol, n_items);
       strcpy(aux, compile_type_str(location));
-      if (location == QUDA_CUDA_FIELD_LOCATION && device::use_constant_memory_arg<>::value) strcat(aux, "cmem,");
+      if (location == QUDA_CUDA_FIELD_LOCATION && use_constant_memory()) strcat(aux, "cmem,");
       if (this->location == QUDA_CPU_FIELD_LOCATION) strcat(aux, getOmpThreadStr());
     }
 

include/device.h
@@ -122,17 +122,6 @@ namespace quda
      */
     unsigned int max_blocks_per_processor();
 
-    /**
-       @brief This helper template class indicated if the present
-       compilation unit has explicit constant memory usage enabled.
-       This is the default "false" specialization, and can be
-       overriden if we include "constant_kernel_arg.h" in our kernel
-       functor file.
-    */
-    template <bool = true> struct use_constant_memory_arg {
-      static constexpr bool value = false;
-    };
-
     namespace profile
     {
 

include/targets/cuda/constant_kernel_arg.h
@@ -16,6 +16,9 @@
 
 #ifndef QUDA_LARGE_KERNEL_ARG
 
+// set a preprocessor flag that we have included constant_kernel_arg.h
+#define QUDA_USE_CONSTANT_MEMORY
+
 namespace quda
 {
 
@@ -50,16 +53,6 @@ namespace quda
     {
       return qudaGetSymbolAddress(buffer);
     }
-
-    /**
-       @brief This helper template class indicates if the present
-       compilation unit has explicit constant memory usage enabled.
-       This is the "true" specialization which is enabled when this
-       header is included.
-    */
-    template <> struct use_constant_memory_arg<true> {
-      static constexpr bool value = true;
-    };
 
   } // namespace device
 

include/targets/hip/constant_kernel_arg.h
@@ -12,6 +12,9 @@
    instantiation correctly.
  */
 
+// set a preprocessor flag that we have included constant_kernel_arg.h
+#define QUDA_USE_CONSTANT_MEMORY
+
 namespace quda
 {
 
@@ -40,16 +43,6 @@ namespace quda
     {
       return qudaGetSymbolAddress(buffer);
     }
-
-    /**
-       @brief This helper template class indicates if the present
-       compilation unit has explicit constant memory usage enabled.
-       This is the "true" specialization which is enabled when this
-       header is included.
-    */
-    template <> struct use_constant_memory_arg<true> {
-      static constexpr bool value = true;
-    };
 
   } // namespace device
 

commit 924b86454e1121dc53ec24151577f751e3e3e031

Fix constant memory tuneKey labeling in hip
It was broken since fad144bad5.

include/targets/hip/tunable_kernel.h
@@ -75,7 +75,7 @@ namespace quda
     {
       strcpy(vol, field.VolString().c_str());
       strcpy(aux, compile_type_str(field, location));
-      if (this->location == QUDA_CUDA_FIELD_LOCATION && device::use_constant_memory_arg<>::value) strcat(aux, "cmem,");
+      if (this->location == QUDA_CUDA_FIELD_LOCATION && use_constant_memory()) strcat(aux, "cmem,");
       if (this->location == QUDA_CPU_FIELD_LOCATION) strcat(aux, getOmpThreadStr());
       strcat(aux, field.AuxString().c_str());
     }
@@ -84,7 +84,7 @@ namespace quda
     {
       u64toa(vol, n_items);
       strcpy(aux, compile_type_str(location));
-      if (location == QUDA_CUDA_FIELD_LOCATION && device::use_constant_memory_arg<>::value) strcat(aux, "cmem,");
+      if (location == QUDA_CUDA_FIELD_LOCATION && use_constant_memory()) strcat(aux, "cmem,");
       if (this->location == QUDA_CPU_FIELD_LOCATION) strcat(aux, getOmpThreadStr());
     }
 